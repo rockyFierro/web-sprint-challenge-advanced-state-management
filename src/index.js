ef8876a3c4645d61838e707d13c78fc24d02a443
rockyFierro <finnslighthouse@gmail.com>
2021-04-04 18:53:57 -0700

    configured redux store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import "./index.css";
 import App from "./App";
+//redux
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import reducer from './reducers';
+import thunk from 'redux-thunk'
+
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)))
 
 const { worker } = require('./mocks/browser');
 worker.start();
@@ -10,7 +17,9 @@ worker.start();
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
-    <App />, 
+    <Provider store={store}>
+        <App />
+    </Provider>,
     rootElement
 );
 
